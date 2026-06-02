a5544710ea005bebd58e84e1efed06fb358083ab
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-02 13:55:10 +0530

    pattern 5 done

--- pattern5.cpp
+++ pattern5.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int row,num, i, j;
+    cout<<"Enter number of rows: ";
+    cin>>row;
+    
+    num=row;
+
+    for (i=1;i<=row;i++) {
+        for (j=1;j<=row;j++) {
+            cout<<num<<" ";
+            num--;
+        }
+        num=row;
+        cout<<"\n";
+    }
+    return 0;
+}
 
