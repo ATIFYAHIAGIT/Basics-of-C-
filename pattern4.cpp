057c3135939fb8ce4d763d065422a05586da1539
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-02 13:49:16 +0530

    pattern 4 done

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int row,num, col, i, j;
+    cout<<"Enter number of rows: ";
+    cin>>row;
+    cout<<"Enter number of columns: ";
+    cin>>col;
+    
+    num=1;
+
+    for (i=1;i<=row;i++) {
+        for (j=1;j<=col;j++) {
+            cout<<num<<" ";
+            num++;
+        }
+        num=1;
+        cout<<"\n";
+    }
+    return 0;
+}
