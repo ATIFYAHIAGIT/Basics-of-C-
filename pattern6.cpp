22e86ac0cf2121c5c16bc8470b660112213a46ac
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-03 23:19:08 +0530

    .

--- pattern6.cpp
+++ pattern6.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int col,row,i,j;
+    cout<<"Enter row: ";
+    cin>>row;
+    cout<<"Enter col: ";
+    cin>>col;
+    char c='A';
+
+    for (i=1;i<=row;i++) {
+        for (j=1;j<=col;j++) {
+            cout<<c<<" ";
+            c=c+1;
+        }
+        cout<<"\n";
+        c='A';
+        }
+    return 0;  
+}
