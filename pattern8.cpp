f300db23c5192418b564e2f7893fff34a11e9901
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-03 23:31:41 +0530

    patterned

--- pattern8.cpp
+++ pattern8.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int row,col,i,j,num;
+    cout<<"Enter row: ";
+    cin>>row;
+    cout<<"Enter col: ";
+    cin>>col;
+    num=1;
+
+    for (i=1;i<=row;i++) {
+        for (j=1;j<=col;j++) {
+            cout<<num<<" ";
+            num=num+1;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
