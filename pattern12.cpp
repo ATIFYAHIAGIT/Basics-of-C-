d2bfb477b2161a7e6fb6cbc2a8317d6d48b06edd
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-04 19:32:07 +0530

    reverse numbers printed

--- pattern12.cpp
+++ pattern12.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int row,col=1,i,j,num;
+    cout<<"Enter row: ";
+    cin>>row;
+    
+    num=1;
+
+    for (i=1;i<=row;i++) {
+        for (j=1;j<=col;j++) {
+            cout<<num<<" ";
+            num=num-1;
+        } 
+        cout<<"\n"; col++;
+        num=col;
+    }
+    return 0;
+}
