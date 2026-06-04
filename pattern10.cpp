c7cf6f4a884a7452f87a9fa831731ded499210f3
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-04 13:50:15 +0530

    pattern pprinted successfully

--- pattern10.cpp
+++ pattern10.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int row,col,i,j,num;
+    cout<<"Enter row: ";
+    cin>>row;
+    col=1;
+    
+    num=1;
+
+    for  (i=1;i<=row;i++) {
+        for (j=1;j<=col;j++) {
+            cout<<num;
+            num=num+1;
+            cout<<" ";
+        }
+        cout<<"\n";
+        num=1;
+        col++;
+    }
+    return 0;
+}
