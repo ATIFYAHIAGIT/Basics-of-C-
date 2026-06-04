3b689b20bf9d2f7b3076d81753940944b9b20478
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-04 19:22:27 +0530

    done

--- pattern11.cpp
+++ pattern11.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num,col,row,i,j;
+    cout<<"Enter row: ";
+    cin>>row;
+    col=1;
+    num=1;
+
+    
+    for (i=1;i<=row;i++) {
+        for (j=1;j<=col;j++) {
+            cout<<i<<" "; 
+            num++;
+            
+        }
+        cout<<"\n"; col++;
+    }
+return 0;
+}
 
