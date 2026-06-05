67d8a08b12e9e9827455261979c9e71be33ec4d9
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-05 20:28:58 +0530

    patterns printed

--- pattern16.cpp
+++ pattern16.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main ()  {
+    int row,col,i,j;
+    cout<<"Enter number of rows: ";
+    cin>>row;
+    col=1;
+
+    for (i=1;i<=row;i++) {
+        for (j=1;j<=col;j++) {
+            cout<<row-j+1<<" ";
+        }
+        cout<<"\n";col++;
+    }
+    return 0;
+}
+            
--- pattern17.cpp
+++ pattern17.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main (){
+    int row,col,i,j,loc,num;
+    cout<<"Enter number of rows: ";
+    cin>>row;
+    col=row;
+    loc=0;
+
+    for (i=row;i>=1;i--) {
+        for(j=col;j>=1;j--) {
+            cout<<"  ";
+        }
+        for (j=1;j<=loc;j=j+1) {
+            cout<<"* ";
+        }
+        cout<<"*";
+        cout<<"\n";
+        col--;
+        loc++;
+}
+    return 0;
+}
