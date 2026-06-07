2298045eda352590189df23a49e0bf1d6ff2e3c8
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-07 16:22:22 +0530

    Stylish H using stars printed

--- pattern27.cpp
+++ pattern27.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int x;
+    cout<<"Enter number of rows for stylish H pattern: ";
+    cin>>x;
+
+    for (int i=1;i<=x;i++) {
+        for(int j=1;j<=i;j++) {
+            cout<<"* ";
+        }
+        for(int j=1;j<=x-i;j++) {
+            cout<<"    ";
+        }
+        for(int j=1;j<=i;j++) {
+            cout<<"* "; 
+        }
+        cout<<"\n";
+    }
+
+    for (int i=1;i<=x;i++) {
+        for(int j=1;j<=x-i;j++) {
+            cout<<"* ";
+        }
+        for(int j=1;j<=i;j++) {
+            cout<<"    ";
+        }
+        for(int j=1;j<=x-i;j++) {
+            cout<<"* "; 
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
