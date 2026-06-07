38d29b0ba078554edc4174ee2b2b4d4a40844c72
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-08 01:52:14 +0530

    last pattern printed hopefully

--- pattern28.cpp
+++ pattern28.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int x;
+    cout<<"Enter number: ";
+    cin>>x;
+
+    for (int i=1;i<=x;i++) {
+        for (int j=1;j<=x-i;j++) {
+            cout<<" ";
+        }
+        for (int j=1;j<=i;j++) {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    for (int i=1;i<=x;i++) {
+        for (int j=1;j<=i;j++) {
+            cout<<" ";
+        }
+        for (int j=1;j<=x-i;j++) {
+            cout<<"* ";
+        }  
+        cout<<"\n";
+    }
+    return 0;
+}
