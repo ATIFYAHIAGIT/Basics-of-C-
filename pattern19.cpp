478be13ada5c9cdd04303401fbc9606fe3a88c7e
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-07 11:24:12 +0530

    right ppyramid pattern printed

--- pattern19.cpp
+++ pattern19.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 int main () {
-    int r,c,n;
+    int r,c,n,x;
     cout<<"Enter your number: ";
     cin>>n;
+    x=0;
 
-    for(r=1;r<=c;r++) {
-        for(r=1)
+    for(r=1;r<=n+1;r++) {
+        for(c=1;c<=n-r+1;c++) {
+            cout<<" ";
+        }
+        for  (c=1;c<=x;c++) {
+            cout<<x;
+        }
+        cout<<"\n";
+        x=r;
     }
 }
