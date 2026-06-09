6333df82e3471304e8634baf1ee5af3a92213c2f
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-09 12:12:33 +0530

    sum of numbers using whileloop

--- whileloop2.cpp
+++ whileloop2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int i,n,sum;
+    cout<<"Enter your number: ";
+    cin>>n;
+
+    i=1;
+    sum=0;
+    while (i<=n) {
+        sum=sum+i;
+        i++;
+    }
+    cout<<sum;
+    return 0;
+}
