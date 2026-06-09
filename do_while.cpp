f4fcbc22e082650aefff80391d0b96d528f3549e
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-09 15:34:14 +0530

    switch and do while done, but switched

--- do_while.cpp
+++ do_while.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main (){
+    int i,integer;
+    cout<<"Enter you number: ";
+    cin>>integer;
+
+    switch (integer) {
+        case 1:
+        cout<<"apple";
+        break;
+
+        case 2:
+        cout<<"bapple";
+        break;
+
+        case 3:
+        cout<<"capple";
+        break;
+
+        default :
+        cout<<"defaulapple";
+    }
+}
--- switch.cpp
+++ switch.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int i,x;
+    cout<<"Enter your number: ";
+    cin>>x;
+
+    i=1;
+
+    do {
+        cout<<i<<" ";
+        i++;
+    } while (i<=x);
+    return 0;
+}
+
 
