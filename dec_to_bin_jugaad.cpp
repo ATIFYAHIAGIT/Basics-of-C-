9a196c9f6d9d34f8257f82ee4bd5f1ee961ae64f
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-10 01:07:49 +0530

    decimal to binary in 2 methods

--- dec_to_bin_jugaad.cpp
+++ dec_to_bin_jugaad.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main () {
+    int i,p,j,r,t;
+    double n,m;
+    i=1,p=0;
+   
+    cout<<"Enter the number: ";
+    cin>>n;
+    
+    m = n; 
+
+
+    while (n>0) {
+        while (i<n) {
+            if((n/i)>=0) {
+            p++;
+            i=i*2;
+            }
+        }
+       
+        n=n/2; 
+    }
+    cout<<"Digits (p): "<<p<<"\n";
+
+    vector<int> arr(p , 0);
+
+    
+    t = (int)m; 
+    
+    for (int k = p - 1; k >= 0; k--) {
+        r = t % 2;
+        
+        if (r != 0) {
+            arr[k] = 1;  
+        } else {
+            arr[k] = 0;  
+        }
+        
+        t = t / 2; 
+    }
+    
+    cout << "Binary Sequence: ";
+    for (int k = 0; k < p; k++) {
+        cout << arr[k];
+    }
+    cout << "\n";
+
+    return 0;
+}
--- dec_to_bin_standard.cpp
+++ dec_to_bin_standard.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int n,rem,bin,dig;
+    bin=0,dig=1;
+
+    cout<<"Enter the number in decimal form:  ";
+    cin>>n;
+
+    while (n>0) {
+        rem=n%2;
+        n=n/2;
+        bin=rem*dig + bin;
+        dig=dig*10;
+    }
+    cout<<"The Binary form of the entered decimal number is: "<<bin;
+    return 0;
+}
