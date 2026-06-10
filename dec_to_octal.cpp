31b0cf02ac299a6f8c68c425056a5c0a89d507fe
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-10 23:21:24 +0530

    conversion done

--- dec_to_octal.cpp
+++ dec_to_octal.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int n,rem,octal=0,pval=1;
+    cout<<"Enter your number: ";
+    cin>>n;
+
+    while (n>0) {
+        rem=n%8;
+        n=n/8;
+        octal = rem*pval + octal;
+        pval=pval*10;
+    }
+    cout<<octal;
+    return 0;
+}
--- octal_to_dec.cpp
+++ octal_to_dec.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int n,rem,dec,pval;
+    dec=0,rem=0;pval=1;
+    cout<<"Enter your number: ";
+    cin>>n;
+
+    while (n>0) {
+        rem=n%10;
+        n=n/10;
+        dec=rem*pval +dec;
+        pval=8*pval;
+    }
+    cout<<dec;
+    return 0;
+}
