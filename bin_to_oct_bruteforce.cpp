d64c201b51f38b8f96d1565827cd26d92cc4fd8c
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-11 00:42:31 +0530

    bin to oct

--- bin_to_oct_bruteforce.cpp
+++ bin_to_oct_bruteforce.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main (){
-    int n,rem,decimal=0,two,x,octal=0,pval=1;
-    two=1;
-    cout<<"Enter the binary number: ";
-    cin>>n;
+int main() {
+    string bin;
+    cout << "Enter binary number: ";
+    cin >> bin;
 
-    while (n>0) {
-        rem=n%10;
-        n=n/10;
-        decimal= rem*two + decimal;
-        two=2*two;
-    }
-    x=decimal;
+    while (bin.length() % 3 != 0)
+        bin = "0" + bin;
 
-    while (x>0) {
-        rem=x%8;
-        x=x/8;
-        octal = rem*pval + octal;
-        pval=pval*10;
+    for (int i = 0; i < bin.length(); i += 3) {
+        int num = (bin[i] - '0') * 4 +
+                  (bin[i + 1] - '0') * 2 +
+                  (bin[i + 2] - '0');
+        cout << num;
     }
-    cout<<octal;
+
+    return 0;
 }
