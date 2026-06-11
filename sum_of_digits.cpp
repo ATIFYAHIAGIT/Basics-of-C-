2aa1eae81722a013e6bfae9218c3b871911b3937
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-11 20:11:50 +0530

    first leetcode probelm solved

--- sum_of_digits.cpp
+++ sum_of_digits.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int x,sum=0;
+    cout<<"Enter your number: ";
+    cin>>x;
+
+    while (x>0) {
+        sum=x%10 + sum;
+        x=x/10;
+    }
+    cout<<sum;
+    return 0;
+}
